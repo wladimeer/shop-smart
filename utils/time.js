04e09720d84ad6e4529a3bf8d5e0541c78e9939d
wladimeer <sbenavidescartes@gmail.com>
2024-07-30 21:59:17 -0400

    Time functions improvements

--- utils/time.js
+++ utils/time.js
@@ -1,29 +1,50 @@
 import moment from 'moment'
+import { DATE_FORMAT, DATE_TEXT_FORMAT, DATETIME_FORMAT } from '../constants/datas'
 
 const getCurrentDatetime = () => {
-  const currentDatetime = moment().format('DD-MM-YYYY hh:mm:ss')
+  const currentDatetime = moment().format(DATETIME_FORMAT)
   return currentDatetime.toString()
 }
 
 const isSameDatetime = (first, second) => {
-  const firstDatetime = moment(first, 'DD-MM-YYYY hh:mm:ss')
-  const secondDatetime = moment(second, 'DD-MM-YYYY hh:mm:ss')
+  const firstDatetime = moment(first, DATETIME_FORMAT)
+  const secondDatetime = moment(second, DATETIME_FORMAT)
 
   return firstDatetime.isSame(secondDatetime)
 }
 
 const isBeforeDatetime = (first, second) => {
-  const firstDatetime = moment(first, 'DD-MM-YYYY hh:mm:ss')
-  const secondDatetime = moment(second, 'DD-MM-YYYY hh:mm:ss')
+  const firstDatetime = moment(first, DATETIME_FORMAT)
+  const secondDatetime = moment(second, DATETIME_FORMAT)
 
   return firstDatetime.isBefore(secondDatetime)
 }
 
 const isAfterDatetime = (first, second) => {
-  const firstDatetime = moment(first, 'DD-MM-YYYY hh:mm:ss')
-  const secondDatetime = moment(second, 'DD-MM-YYYY hh:mm:ss')
+  const firstDatetime = moment(first, DATETIME_FORMAT)
+  const secondDatetime = moment(second, DATETIME_FORMAT)
 
   return firstDatetime.isAfter(secondDatetime)
 }
 
-export { getCurrentDatetime, isSameDatetime, isBeforeDatetime, isAfterDatetime }
+const formatToDate = (datetime) => {
+  return moment(datetime).format(DATE_FORMAT)
+}
+
+const formatToText = (datetime) => {
+  return moment(datetime, DATETIME_FORMAT).format(DATE_TEXT_FORMAT)
+}
+
+const fromUntilNow = (datetime) => {
+  return moment(datetime).fromNow()
+}
+
+export {
+  getCurrentDatetime,
+  isSameDatetime,
+  isBeforeDatetime,
+  isAfterDatetime,
+  formatToDate,
+  formatToText,
+  fromUntilNow
+}

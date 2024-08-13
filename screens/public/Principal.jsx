48fb0ff173aa8bfe3fd34d381ebcd0658cb23292
wladimeer <sbenavidescartes@gmail.com>
2024-08-13 19:26:16 -0400

    Version indicator only to paid variant added

--- screens/public/Principal.jsx
+++ screens/public/Principal.jsx
@@ -2,6 +2,7 @@ import ElementGroup from '../../components/ElementGroup'
 import { PRINCIPAL_SCREEN_KEY } from '../../constants/screens'
 import VersionIndicator from '../../components/VersionIndicator'
 import { VIEW_PURCHASES_SCREEN_KEY } from '../../constants/screens'
+import { APP_VARIANT, PAID_VARIANT } from '../../constants/config'
 import { NEW_PURCHASE_SCREEN_KEY } from '../../constants/screens'
 import ScreenContainer from '../../components/ScreenContainer'
 import background from '../../assets/principal-background.jpg'
@@ -10,6 +11,8 @@ import { useTranslation } from 'react-i18next'
 
 const Principal = ({ navigation }) => {
   const [translate] = useTranslation(PRINCIPAL_SCREEN_KEY)
+
+  const isPaidVariant = APP_VARIANT === PAID_VARIANT
 
   const handleNewPurchase = () => {
     navigation.navigate(NEW_PURCHASE_SCREEN_KEY)
@@ -31,7 +34,7 @@ const Principal = ({ navigation }) => {
         </ElementGroup>
       </ScreenContainer>
 
-      <VersionIndicator />
+      {isPaidVariant && <VersionIndicator />}
     </>
   )
 }

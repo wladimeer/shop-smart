e70d12608e9e195b66203db8f995d9627270676b
wladimeer <sbenavidescartes@gmail.com>
2025-04-14 21:37:38 -0400

    Refactor and fix in statistics view

--- screens/private/Statistics.jsx
+++ screens/private/Statistics.jsx
@@ -19,13 +19,11 @@ const Statistics = ({ navigation }) => {
   const insets = useSafeAreaInsets()
   const { width: screenWidth } = Dimensions.get('window')
   const [translate] = useTranslation(SCREEN_KEYS.STATISTICS)
-  const [highestQuantityItems, setHighestQuantityItems] = useState([])
-  const [quantityThroughTime, setQuantityThroughTime] = useState([])
-  const [priceThroughTime, setPriceThroughTime] = useState([])
+  const [purchases, setPurchases] = useState([])
   const [loading, setLoading] = useState(true)
   const { colors } = useTheme()
 
-  const styles = allStyles()
+  const styles = allStyles({ colors })
 
   const pieChartConfig = {
     color: () => colors.denary
@@ -43,93 +41,94 @@ const Statistics = ({ navigation }) => {
     try {
       const data = await getElementsList()
 
-      const groupedByMonth = {}
-      const groupedByName = {}
-
-      data.forEach(({ elements, createdAt }) => {
-        const monthName = getMonthNameFromDatetime(createdAt)
-        const monthNumber = getMonthNumberFromDatetime(createdAt)
-
-        elements.forEach(({ name, quantity, unit }) => {
-          groupedByName[name] ??= {
-            name,
-            quantity: 0,
-            unit
-          }
-
-          groupedByName[name]['quantity'] += Number(quantity)
-
-          groupedByMonth[monthNumber] ??= {
-            monthName,
-            monthNumber,
-            quantities: 0,
-            prices: 0
-          }
-
-          groupedByMonth[monthNumber]['quantities'] += Number(quantity)
-          groupedByMonth[monthNumber]['prices'] += Number(quantity) * Number(unit)
-        })
-      })
-
-      const generateColor = uniqueColorGenerator()
-
-      const sortedByQuantity = Object.values(groupedByName).sort((a, b) => {
-        const result = a.quantity > b.quantity
-        return result ? -1 : !result ? 1 : 0
-      })
-
-      const highestQuantityItems = sortedByQuantity.slice(0, 5).map((item) => ({
-        name: item.name,
-        population: item.quantity,
-        legendFontColor: colors.text,
-        color: generateColor(),
-        legendFontSize: 15
-      }))
-
-      setHighestQuantityItems(highestQuantityItems)
-
-      const sortedByDate = Object.values(groupedByMonth).sort((a, b) => {
-        const result = a.monthNumber < b.monthNumber
-        return result ? -1 : !result ? 1 : 0
-      })
-
-      const quantityThroughTime = {
-        labels: sortedByDate.map((v) => v.monthName),
-        datasets: [
-          {
-            data: sortedByDate.map((v) => v.quantities),
-            color: () => colors.senary,
-            strokeWidth: 2
-          }
-        ],
-        legend: [translate('indicators.monthQuantity')]
-      }
-
-      setQuantityThroughTime(quantityThroughTime)
-
-      const priceThroughTime = {
-        labels: sortedByDate.map((v) => v.monthName),
-        datasets: [
-          {
-            data: sortedByDate.map((v) => v.prices),
-            color: () => colors.senary,
-            strokeWidth: 2
-          }
-        ],
-        legend: [translate('indicators.monthPrice')]
-      }
-
-      setPriceThroughTime(priceThroughTime)
-
+      setPurchases(data)
       setLoading(false)
     } catch (error) {
       console.log(error)
     }
   }
 
+  const groupedByMonth = {}
+  const groupedByName = {}
+
+  purchases.forEach(({ elements, createdAt }) => {
+    const monthName = getMonthNameFromDatetime(createdAt)
+    const monthNumber = getMonthNumberFromDatetime(createdAt)
+
+    elements.forEach(({ name, quantity, unit }) => {
+      groupedByName[name] ??= {
+        name,
+        quantity: 0,
+        unit
+      }
+
+      groupedByName[name]['quantity'] += Number(quantity)
+
+      groupedByMonth[monthNumber] ??= {
+        monthName,
+        monthNumber,
+        quantities: 0,
+        prices: 0
+      }
+
+      groupedByMonth[monthNumber]['quantities'] += Number(quantity)
+      groupedByMonth[monthNumber]['prices'] += Number(quantity) * Number(unit)
+    })
+  })
+
+  const generateColor = uniqueColorGenerator()
+
+  const sortedByQuantity = Object.values(groupedByName).sort((a, b) => {
+    const result = a.quantity > b.quantity
+    return result ? -1 : !result ? 1 : 0
+  })
+
+  const highestQuantityItems = sortedByQuantity.slice(0, 5).map((item) => ({
+    name: item.name,
+    population: item.quantity,
+    legendFontColor: colors.text,
+    color: generateColor(),
+    legendFontSize: 15
+  }))
+
+  const sortedByDate = Object.values(groupedByMonth).sort((a, b) => {
+    const result = a.monthNumber < b.monthNumber
+    return result ? -1 : !result ? 1 : 0
+  })
+
+  const quantityThroughTime = {
+    labels: sortedByDate.map((v) => v.monthName),
+    datasets: [
+      {
+        data: sortedByDate.map((v) => v.quantities),
+        color: () => colors.senary,
+        strokeWidth: 2
+      }
+    ],
+    legend: [translate('indicators.monthQuantity')]
+  }
+
+  const priceThroughTime = {
+    labels: sortedByDate.map((v) => v.monthName),
+    datasets: [
+      {
+        data: sortedByDate.map((v) => v.prices),
+        color: () => colors.senary,
+        strokeWidth: 2
+      }
+    ],
+    legend: [translate('indicators.monthPrice')]
+  }
+
   useEffect(() => {
     handleLoadCharts()
   }, [])
+
+  const ListEmpty = () => (
+    <View style={styles.messageContent}>
+      <CustomText text={translate('indicators.noData')} color={colors.text} />
+    </View>
+  )
 
   return (
     <ScreenContainer noSafeArea={true}>
@@ -137,71 +136,79 @@ const Statistics = ({ navigation }) => {
         <ActivityIndicator size="large" />
       ) : (
         <ScrollView style={styles.container}>
-          <View style={styles.chartContainer}>
-            <View style={styles.chartTitle}>
-              <CustomText
-                text={translate('indicators.highestQuantity')}
-                color={colors.secondary}
-                size={22}
-              />
+          {purchases.length > 0 ? (
+            <>
+              <View style={styles.chartContainer}>
+                <View style={styles.chartTitle}>
+                  <CustomText
+                    text={translate('indicators.highestQuantity')}
+                    color={colors.secondary}
+                    size={22}
+                  />
+                </View>
+
+                <PieChart
+                  width={screenWidth - 20}
+                  data={highestQuantityItems}
+                  backgroundColor={colors.background}
+                  chartConfig={pieChartConfig}
+                  style={styles.chartElement}
+                  accessor={'population'}
+                  paddingLeft={-40}
+                  center={[40, 0]}
+                  height={220}
+                />
+              </View>
+
+              <Spacer color="transparent" size={18} />
+
+              <View style={styles.chartContainer}>
+                <View style={styles.chartTitle}>
+                  <CustomText
+                    text={translate('indicators.quantityThroughTime')}
+                    color={colors.secondary}
+                    size={22}
+                  />
+                </View>
+
+                <LineChart
+                  fromZero={true}
+                  width={screenWidth - 20}
+                  data={quantityThroughTime}
+                  chartConfig={lineChartConfig}
+                  style={styles.chartElement}
+                  segments={3}
+                  height={220}
+                />
+              </View>
+
+              <Spacer color="transparent" size={18} />
+
+              <View style={styles.chartContainer}>
+                <View style={styles.chartTitle}>
+                  <CustomText
+                    text={translate('indicators.priceThroughTime')}
+                    color={colors.secondary}
+                    size={22}
+                  />
+                </View>
+
+                <LineChart
+                  fromZero={true}
+                  width={screenWidth - 20}
+                  data={priceThroughTime}
+                  chartConfig={lineChartConfig}
+                  style={styles.chartElement}
+                  segments={3}
+                  height={220}
+                />
+              </View>
+            </>
+          ) : (
+            <View style={styles.chartContainer}>
+              <ListEmpty />
             </View>
-
-            <PieChart
-              width={screenWidth - 20}
-              data={highestQuantityItems}
-              backgroundColor={colors.background}
-              chartConfig={pieChartConfig}
-              style={styles.chartElement}
-              accessor={'population'}
-              paddingLeft={-40}
-              center={[40, 0]}
-              height={220}
-            />
-          </View>
-
-          <Spacer color="transparent" size={18} />
-
-          <View style={styles.chartContainer}>
-            <View style={styles.chartTitle}>
-              <CustomText
-                text={translate('indicators.quantityThroughTime')}
-                color={colors.secondary}
-                size={22}
-              />
-            </View>
-
-            <LineChart
-              fromZero={true}
-              width={screenWidth - 20}
-              data={quantityThroughTime}
-              chartConfig={lineChartConfig}
-              style={styles.chartElement}
-              segments={3}
-              height={220}
-            />
-          </View>
-
-          <Spacer color="transparent" size={18} />
-
-          <View style={styles.chartContainer}>
-            <View style={styles.chartTitle}>
-              <CustomText
-                text={translate('indicators.priceThroughTime')}
-                color={colors.secondary}
-                size={22}
-              />
-            </View>
-
-            <LineChart
-              fromZero={true}
-              width={screenWidth - 20}
-              data={priceThroughTime}
-              chartConfig={lineChartConfig}
-              style={styles.chartElement}
-              segments={3}
-              height={220}
-            />
-          </View>
+          )}
 
           <Spacer color={colors.primary} size={insets.bottom || DEFAULT_INSETS.BOTTOM} />
         </ScrollView>
@@ -210,7 +217,7 @@ const Statistics = ({ navigation }) => {
   )
 }
 
-const allStyles = () => {
+const allStyles = ({ colors }) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -227,6 +234,13 @@ const allStyles = () => {
     },
     chartElement: {
       borderRadius: 3
+    },
+    messageContent: {
+      width: '100%',
+      backgroundColor: colors.background,
+      marginVertical: 5,
+      borderRadius: 3,
+      padding: 20
     }
   })
 
